be01ac16f6522e415ac72ceb606dfe5e6397588e
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-05-25 21:03:39 -0700

    Refactor

--- src/modal.js
+++ src/modal.js
@@ -1,4 +1,4 @@
-import { createElement } from "./helpers"
+import { createElement, priorityClass } from "./helpers"
 
 const renderModalCard = (task) => {
   const modal = createElement({ tag: "div", className: "modal is-active", id: "modal" })
@@ -8,29 +8,40 @@ const renderModalCard = (task) => {
     eventListener: ["click", clearModal]
   })
   const close = createElement({ tag: "button", className: "modal-close is-large" })
-  const card = createElement({ tag: "article", className: "card" })
-  const cardHead = createElement({ tag: "header", className: "card-header" })
-  const cardTitle = createElement({
-    tag: "p",
-    className: "card-header-title",
-    content: task.name
-  })
-  const cardBody = createElement({ tag: "div", className: "card-content" })
-  const cardFoot = createElement({ tag: "footer", className: "card-footer" })
+  const card = composeTaskCard(task)
 
   modal.appendChild(background)
   modal.appendChild(card)
   modal.appendChild(close)
-  card.appendChild(cardHead)
-  card.appendChild(cardBody)
-  card.appendChild(cardFoot)
-  cardHead.appendChild(cardTitle)
 
   document.body.appendChild(modal)
 }
 
 const clearModal = () => {
   document.getElementById("modal").remove()
+}
+
+const composeTaskCard = (task) => {
+  const card = createElement({ tag: "article", className: "card" })
+  const cardHead = createElement({ tag: "header", className: "card-header level" })
+  const cardTitle = createElement({
+    tag: "p",
+    className: "card-header-title",
+    content: task.name
+  })
+  const cardBody = createElement({
+    tag: "div",
+    className: "card-content",
+    content: task.description
+  })
+  const cardFoot = createElement({ tag: "footer", className: "card-footer" })
+
+  card.appendChild(cardHead)
+  card.appendChild(cardBody)
+  card.appendChild(cardFoot)
+  cardHead.appendChild(cardTitle)
+
+  return card
 }
 
 export { renderModalCard }
