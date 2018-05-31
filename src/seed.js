068fcbee60fe01b8479d980230bf00cc89644f58
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-05-31 13:54:45 -0700

    Update dates in seed file

--- src/seed.js
+++ src/seed.js
@@ -1,56 +1,58 @@
-const seed = (projects) => {
+const seed = projects => {
   projects.createProject("Welcome")
   projects.createProject("Empty")
 
   projects.project0.createBoard("Get started")
   projects.project0.createBoard("This is a board")
 
+  const dueDate = Date.now() + 1000 * 60 * 60 * 24
+
   projects.project0.board0.createTask({
     name: "Create a task",
-    dueDate: "5/5/2019",
+    dueDate: dueDate,
     priority: "low"
   })
 
   projects.project0.board0.createTask({
     name: "Create a board",
-    dueDate: "5/5/2019",
+    dueDate: dueDate,
     priority: "medium"
   })
 
   projects.project0.board0.createTask({
     name: "Create a project",
-    dueDate: "5/5/2019",
+    dueDate: dueDate,
     priority: "high"
   })
 
   projects.project0.board0.createTask({
     name: "You can do it",
-    dueDate: "5/5/2019",
+    dueDate: dueDate,
     priority: "urgent"
   })
 
   projects.project0.board1.createTask({
     name: "This is a task",
-    dueDate: "5/5/2019",
+    dueDate: dueDate,
     priority: "low"
   })
 
   projects.project0.board1.createTask({
     name: "Tasks can have descriptions",
     description: "Like this one!",
-    dueDate: "5/5/2019",
+    dueDate: dueDate,
     priority: "low"
   })
 
   projects.project0.board1.createTask({
     name: "And different priorities",
-    dueDate: "5/5/2019",
+    dueDate: dueDate,
     priority: "urgent"
   })
 
   projects.project0.board1.createTask({
     name: "And due dates",
-    dueDate: "5/5/2019",
+    dueDate: dueDate,
     priority: "low"
   })
 }
